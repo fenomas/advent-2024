39b2d766401d7eed6b4e5c9b73c9c4a58208cab2
fenomas <andy@fenomas.com>
2024-12-07 00:43:08 +0900

    cleanup

--- src/advent/day05/solution.ts
+++ src/advent/day05/solution.ts
@@ -6,7 +6,9 @@ export const answers = [
 //
 //
 
-export const part1 = (input = '') => {
+export const part1 = (input = '') => part1_impl(input).total
+
+export const part1_impl = (input = '') => {
   const [rulePairs, data] = input.split('\n\n')
 
   const follows = new Map()
@@ -17,50 +19,30 @@ export const part1 = (input = '') => {
   })
 
   let total = 0
-
-  data.split('\n').forEach((line) => {
-    const nums = line.split(',').reverse()
-    const ng = new Set()
-    const ok = nums.every((a) => {
-      if (ng.has(a)) return false
-      ;(follows.get(a) || []).forEach((b = '') => ng.add(b))
-      return true
-    })
-    if (ok) total += +nums[nums.length >> 1]
-  })
-
-  return total
-}
-
-export const part2 = (input = '') => {
-  const [rulePairs, data] = input.split('\n\n')
-
-  const follows = new Map()
-  rulePairs.split('\n').forEach((line) => {
-    const [a, b] = line.split('|')
-    if (!follows.has(a)) follows.set(a, [])
-    follows.get(a).push(b)
-  })
-
-  let total = 0
-
-  data.split('\n').forEach((line) => {
-    const nums = line.split(',').reverse()
-    for (let i = 0; i < 10000; i++) {
+  const wrongSets = data
+    .split('\n')
+    .map((line) => line.split(',').reverse())
+    .filter((nums) => {
       const ng = new Set()
-      const ok = nums.every((a, j) => {
-        if (ng.has(a)) {
-          ;[nums[j], nums[j - 1]] = [nums[j - 1], nums[j]]
-          return false
-        }
+      const ok = nums.every((a) => {
+        if (ng.has(a)) return false
         ;(follows.get(a) || []).forEach((b = '') => ng.add(b))
         return true
       })
-      if (ok && i === 0) return
-      if (ok) break
-    }
-    total += +nums[nums.length >> 1]
-  })
+      if (ok) total += +nums[nums.length >> 1]
+      return !ok
+    })
+  return { total, wrongSets, follows }
+}
 
-  return total
+export const part2 = (input = '') => {
+  const { wrongSets, follows } = part1_impl(input)
+
+  return wrongSets.reduce((sum, nums) => {
+    nums.sort((a, b) => {
+      if (follows.has(a) && follows.get(a).includes(b)) return -1
+      return 1
+    })
+    return sum + +nums[nums.length >> 1]
+  }, 0)
 }
