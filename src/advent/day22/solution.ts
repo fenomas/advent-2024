cf8350a74b929f7e641598236684a6523e8b18fb
fenomas <andy@fenomas.com>
2024-12-22 15:53:52 +0900

    day 22

---
+++ src/advent/day22/solution.ts
@@ -0,0 +1,48 @@
+export const answers = [
+  ['16619522798', '1854'], // real
+  ['37327623', '24'],
+]
+
+//
+//
+
+const mask = 2 ** 24 - 1
+const evolve = (num: number, iter = 1) => {
+  while (iter-- > 0) {
+    num = (num ^ (num << 6)) & mask
+    num = num ^ (num >> 5) // & mask
+    num = (num ^ (num << 11)) & mask
+  }
+  return num
+}
+
+export const part1 = (input = '') =>
+  input //
+    .split('\n')
+    .reduce((acc, line) => acc + evolve(Number(line), 2000), 0)
+
+export const part2 = (input = '') => {
+  const lookup = new Map()
+  const keyMask = (1 << 28) - 1
+
+  input.split('\n').forEach((line) => {
+    const seen = new Set()
+    let num = Number(line)
+    let prev = 0
+    let key = 0
+    for (let i = 0; i < 2000; i++) {
+      num = evolve(num)
+      const price = num % 10
+      const diff = price - prev + 9
+      key = ((key << 7) | diff) & keyMask
+      prev = price
+      if (!seen.has(key) && i >= 5) {
+        seen.add(key)
+        const ct = lookup.get(key) || 0
+        lookup.set(key, ct + price)
+      }
+    }
+  })
+
+  return lookup.values().reduce((acc, val) => Math.max(acc, val), 0)
+}
