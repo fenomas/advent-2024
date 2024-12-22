b3b8363404b72cd3313f476f17df2f5d2a8b02eb
fenomas <andy@fenomas.com>
2024-12-22 16:23:35 +0900

    add favico

---
+++ src/advent/day23/solution.ts
@@ -0,0 +1,29 @@
+export const answers = [
+    ['', ''], // real
+    ['', ''],
+  ]
+  
+  //
+  //
+  
+  export const part1 = (input = '') => {
+    // const lines = input.split('\n').map((line) => line.split(''))
+    // const get = (i = 0, j = 0) => (lines[i] && lines[i][j]) || '-'
+  
+    let total = 0
+  
+    input.split('\n').forEach((line) => {
+      line.split(/ +/).forEach((word) => {
+        //
+        total += word.length
+        //
+      })
+    })
+  
+    return total
+  }
+  
+  export const part2 = (input = '') => {
+    return input.length
+  }
+  
