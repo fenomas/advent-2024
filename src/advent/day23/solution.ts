1b51281c0b604fc163ea6dd3edbf5da52fcb9915
fenomas <andy@fenomas.com>
2024-12-23 19:20:31 +0900

    day 23

--- src/advent/day23/solution.ts
+++ src/advent/day23/solution.ts
@@ -1,29 +1,62 @@
 export const answers = [
-    ['', ''], // real
-    ['', ''],
-  ]
-  
-  //
-  //
-  
-  export const part1 = (input = '') => {
-    // const lines = input.split('\n').map((line) => line.split(''))
-    // const get = (i = 0, j = 0) => (lines[i] && lines[i][j]) || '-'
-  
-    let total = 0
-  
-    input.split('\n').forEach((line) => {
-      line.split(/ +/).forEach((word) => {
-        //
-        total += word.length
-        //
+  ['926', 'az,ed,hz,it,ld,nh,pc,td,ty,ux,wc,yg,zz'], // real
+  ['7', 'co,de,ka,ta'],
+]
+
+//
+//
+
+const parse = (input: string) => {
+  const data = new Map<string, string[]>()
+  input.split('\n').forEach((line) => {
+    const [a, b] = line.split('-').map((s) => s.trim())
+    if (!data.has(a)) data.set(a, [])
+    if (!data.has(b)) data.set(b, [])
+    data.get(a)?.push(b)
+    data.get(b)?.push(a)
+  })
+  return data
+}
+
+export const part1 = (input = '') => {
+  const data = parse(input)
+  const seen = new Set<string>()
+
+  data.keys().forEach((key) => {
+    if (!key.startsWith('t')) return
+    data.get(key)?.forEach((key2) => {
+      if (key2 === key) return
+      data.get(key2)?.forEach((key3) => {
+        if (key3 === key2 || key3 === key) return
+        if (!data.get(key3)?.includes(key)) return
+        seen.add([key, key2, key3].sort().join('-'))
       })
     })
-  
-    return total
+  })
+
+  return seen.size
+}
+
+export const part2 = (input = '') => {
+  const data = parse(input)
+  const seen = new Set<string>()
+  let best = ['-']
+
+  const check = (key: string, currList: string[]) => {
+    const next =
+      data.get(key)?.filter((k) => {
+        return currList.every((c) => data.get(k)?.includes(c))
+      }) || []
+    if (currList.length + next.length < best.length) return
+    next.forEach((k) => {
+      if (seen.has(k)) return
+      const newCurrList = [...currList, k]
+      if (newCurrList.length > best.length) best = newCurrList
+      check(k, newCurrList)
+    })
+    seen.add(key)
   }
-  
-  export const part2 = (input = '') => {
-    return input.length
-  }
-  
+
+  data.keys().forEach((key) => check(key, []))
+  return best.sort().join(',')
+}
